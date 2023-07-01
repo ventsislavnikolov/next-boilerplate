0f3068e8fd83463fcc171da112ab12f38c7cc1fa
Ventsislav Nikolov <34811964+ventsislavnikolov@users.noreply.github.com>
2023-07-01 13:06:06 +0300

    chore(release): add more stuff

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,9 +2,34 @@ import './globals.css';
 
 import { Roboto } from 'next/font/google';
 import { notFound } from 'next/navigation';
-import { NextIntlClientProvider } from 'next-intl';
+import { createTranslator, NextIntlClientProvider } from 'next-intl';
 
 import { ReduxProvider, ToastProvider } from 'providers';
+
+async function getMessages(locale: string) {
+  try {
+    return (await import(`../../translations/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
+}
+
+export async function generateStaticParams() {
+  return ['en'].map((locale) => ({ locale }));
+}
+
+export async function generateMetadata({ params: { locale } }) {
+  const messages = await getMessages(locale);
+
+  // You can use the core (non-React) APIs when you have to use next-intl
+  // outside of components. Potentially this will be simplified in the future
+  // (see https://next-intl-docs.vercel.app/docs/next-13/server-components).
+  const t = createTranslator({ locale, messages });
+
+  return {
+    title: t('title'),
+  };
+}
 
 const roboto = Roboto({
   weight: '400',
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,19 +1,45 @@
 'use client';
 
-import { useTranslations } from 'next-intl';
+import { useRouter } from 'next/navigation';
+import { usePathname } from 'next-intl/client';
+import { useLocale, useTranslations } from 'next-intl';
 
 // UI components
 import { Loader } from 'components';
 import { useGetAbilityQuery } from 'store/services/applicationApi';
 
 export default function Home() {
+  const router = useRouter();
+  const locale = useLocale();
+  const pathname = usePathname();
   const t = useTranslations('Index');
 
   const { isLoading } = useGetAbilityQuery({ limit: 20, offset: 0 });
+
+  function handleButtonClick(value) {
+    router.replace(`/${value}${pathname}`);
+  }
 
   if (isLoading) {
     return <Loader />;
   }
 
-  return <p className='pb-3 text-2xl underline'>{t('hello-boilerplate')}</p>;
+  return (
+    <div className='flex min-h-screen flex-col'>
+      <header className='py-4'>
+        <button
+          type='button'
+          className='float-right mb-2 mr-4 rounded bg-white px-4 py-2 text-gray-800'
+          onClick={() => handleButtonClick(locale === 'en' ? 'en' : 'en')}>
+          {locale === 'en' ? t('english') : t('english')}
+        </button>
+      </header>
+      <main className='flex flex-1 flex-col items-center justify-between p-24'>
+        <p className='pb-3 text-2xl underline'>{t('hello-boilerplate')}</p>
+      </main>
+      <footer className='flex items-center justify-center py-6'>
+        Copyright {new Date().getFullYear()}
+      </footer>
+    </div>
+  );
 }
