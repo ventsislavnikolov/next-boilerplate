7003a5f32b75fc98de2586e99b0202af480f8b79
Ventsislav Nikolov <34811964+ventsislavnikolov@users.noreply.github.com>
2023-07-13 12:01:17 +0300

    chore(release): fix envs

--- env.mjs
+++ env.mjs
@@ -3,11 +3,14 @@ import { z } from 'zod';
 
 const env = createEnv({
   server: {
+    SENTRY_DSN: z.string().optional(),
     SENTRY_ORG: z.string().optional(),
+    NEXTAUTH_URL: z.string().optional(),
     SENTRY_PROJECT: z.string().optional(),
+    GOOGLE_CLIENT_ID: z.string().optional(),
     SENTRY_AUTH_TOKEN: z.string().optional(),
     NEXT_PUBLIC_API_URL: z.string().optional(),
-    NEXT_PUBLIC_SENTRY_DSN: z.string().optional(),
+    GOOGLE_CLIENT_SECRET: z.string().optional(),
     CHROMATIC_PROJECT_TOKEN: z.string().optional(),
     NODE_ENV: z.enum(['development', 'test', 'production']).optional(),
     ANALYZE: z
@@ -19,11 +22,17 @@ const env = createEnv({
   runtimeEnv: {
     ANALYZE: process.env.ANALYZE,
     NODE_ENV: process.env.NODE_ENV,
+    GITHUB_ID: process.env.GITHUB_ID,
+    SENTRY_DSN: process.env.SENTRY_DSN,
     SENTRY_ORG: process.env.SENTRY_ORG,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
     SENTRY_PROJECT: process.env.SENTRY_PROJECT,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     CHROMATIC_PROJECT_TOKEN: process.env.CHROMATIC_PROJECT_TOKEN,
   },
 });
