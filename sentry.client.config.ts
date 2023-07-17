8555e273d16e1622cf6c7fabc99d7bcb783c434e
ventsislavnikolov <ventsislavnikolovv@gmail.com>
2023-07-17 14:29:59 +0300

    chore(release): debug to false

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  debug: true,
+  debug: false,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 1,
   replaysOnErrorSampleRate: 1.0,
