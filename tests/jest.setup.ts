a329ea763aca429462b0d9885cffa70e90cde1ea
Ventsislav Nikolov <ventsislavnikolovv@gmail.com>
2023-09-22 01:26:13 +0300

    chore(update): update next and libraries

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -2,4 +2,3 @@
 // If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
 
 // Learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
