12f9f3e776df3c58e183e21f43e3a13fbbe968e5
Ventsislav Nikolov <ventsislavnikolovv@gmail.com>
2023-09-22 01:27:52 +0300

    chore(update): update next and libraries

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -2,3 +2,4 @@
 // If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
 
 // Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/jest-globals';
