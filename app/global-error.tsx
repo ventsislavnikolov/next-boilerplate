e906df23c0cfdc23badeeb519c74553e6330a2b2
ventsislavnikolov <ventsislavnikolovv@gmail.com>
2024-01-17 11:27:15 +0200

    chore(release): fix build

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,16 +1,14 @@
-'use client'
+'use client';
 
-export default function GlobalError({
-  reset,
-}: {
-  reset: () => void
-}) {
+export default function GlobalError({ reset }: { reset: () => void }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         <h2>Something went wrong!</h2>
-        <button type="button" onClick={() => reset()}>Try again</button>
+        <button type='button' onClick={() => reset()}>
+          Try again
+        </button>
       </body>
     </html>
-  )
+  );
 }
